5eeb51073df4474f08f6262771817ef328e921d4
Shreya <tyagishreya827@gmail.com>
2025-08-07 15:36:09 +0530

    update

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div className='first m-3 text-light text-center'>Copyright@mynewsapp.com</div>
+    <div className='first m-3 text-light text-center'>Copyright@newsapp.com</div>
   )
 }
